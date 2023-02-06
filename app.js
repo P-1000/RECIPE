a8e97fefb9bb1d2add3bbf0298b1429411cf096b
Pavan Patchikarla <pavan.patchikarla@yahoo.com>
2023-02-06 23:06:53 +0530

    appending some data to dom

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const app_key = 'a165f4a5ef68e83ef8cf2e59aa3cf30f';
 const app_id = '3d7f623c';
 
-const query = document.getElementById("query_Holder");
-const search = document.getElementById("S_btn");
+const query = document.querySelector('.search input')
+const search = document.querySelector('.search button')
+
 
 const container = document.querySelector('.container');
 
-
-
-const handleSearch = async ()=>{
+const handleSearch = async()=>{
     try {
+        container.innerHTML=''
         const endPoint = `https://api.edamam.com/search?q=${query.value}&app_id=${app_id}&app_key=${app_key}`
         const response = await fetch(endPoint);
         const {hits} = await response.json();
+        console.log(hits);
+
         hits.map(({recipe})=>{
-            const ele = document.createElement('img');
-            ele.style="width:480px;margin:12px"
-            ele.src = recipe.image;
+            // console.log(recipe)
+            const {image , label, calories, url} = recipe
+            const ele = document.createElement('div');
+            ele.innerHTML= `
+                <div class="card">
+                <img src="${image}" alt="${label}"/>
+                <h2>${label}</h2>
+                <a href="${url}">View Recipe</a>
+                <h3>${calories}</h3>
+                </div>
+            `
             container.appendChild(ele);
         }
         )
+
     } catch (error) {
         console.log(error)
-        console.log("error")
+        console.log("error Occured")
     }
+
 }
 
+
+search.addEventListener('click',handleSearch);
 
