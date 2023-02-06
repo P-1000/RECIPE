c5e9088bdc1a1ef88f20117bbc069d0341fcd44c
Pavan Patchikarla <pavan.patchikarla@yahoo.com>
2023-02-06 22:07:48 +0530

    search handle test

---
+++ app.js
@@ -0,0 +1,26 @@
+const app_key = 'a165f4a5ef68e83ef8cf2e59aa3cf30f';
+const app_id = '3d7f623c';
+
+//search bar 
+
+const query = document.getElementById("query_Holder");
+const search = document.getElementById("S_btn");
+
+const handleSearch=()=>{
+    console.log("searcher")
+}
+
+
+
+// const handleSearch = async ()=>{
+//     try {
+//         const endPoint = `https://api.edamam.com/search?q=${'pizza'}&app_id=${app_id}&app_key=${app_key}`
+//         const response = await fetch(endPoint);
+//         const data = await response.json();
+//         console.log(data )
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+
