8e7ce0ab1771edac8e71d137373952b870d7175c
Pavan Patchikarla <pavan.patchikarla@yahoo.com>
2023-02-06 22:36:21 +0530

    appending imgs to dom

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const app_key = 'a165f4a5ef68e83ef8cf2e59aa3cf30f';
 const app_id = '3d7f623c';
 
-//search bar 
-
 const query = document.getElementById("query_Holder");
 const search = document.getElementById("S_btn");
 
+const container = document.querySelector('.container');
 
 
 
@@ -14,8 +13,12 @@ const handleSearch = async ()=>{
         const endPoint = `https://api.edamam.com/search?q=${query.value}&app_id=${app_id}&app_key=${app_key}`
         const response = await fetch(endPoint);
         const {hits} = await response.json();
-        hits.map(({recipe})=>
-            console.log(recipe)
+        hits.map(({recipe})=>{
+            const ele = document.createElement('img');
+            ele.style="width:480px;margin:12px"
+            ele.src = recipe.image;
+            container.appendChild(ele);
+        }
         )
     } catch (error) {
         console.log(error)
